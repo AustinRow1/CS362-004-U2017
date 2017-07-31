472d4e356ccdcd2fb73b79bd8d75ab2d325366e6
Austin Row <rowa@flip1.engr.oregonstate.edu>
2017-07-31 12:29:14 -0700

    Assignment 5. Added teammate's dominion code directory. Moved and modified my unit and random tests in the directory so that they could be used to test teammate's code.

--- projects/rowa/dominion/dominion.c
+++ projects/rowa/dominion/dominion.c
@@ -677,7 +677,7 @@ int smithyCardEffect(int currentPlayer, struct gameState *state, int handPos)
 	int i;	
 
 	//+3 Cards
-	for (i = 0; i <= 3; i++)
+	for (i = 0; i < 3; i++)
 	{
 		drawCard(currentPlayer, state);
 	}
